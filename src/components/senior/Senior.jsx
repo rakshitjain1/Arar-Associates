ef244acd4e01565c4960108e4f830f58937ae6c6
rakshitjain1 <rakshitjain99919@gmail.com>
2024-09-29 10:22:29 +0530

    second commit

--- src/components/senior/Senior.jsx
+++ src/components/senior/Senior.jsx
@@ -21,7 +21,7 @@ const TestimonialData = [
     img: "https://picsum.photos/104/104",
   },
   {
-    id: 5,
+    id: 4,
     name: "Sachin Tendulkar",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
     img: "https://picsum.photos/103/103",
@@ -54,7 +54,6 @@ const Senior = () => {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          initialSlide: 2,
         },
       },
       {
@@ -69,45 +68,43 @@ const Senior = () => {
 
   return (
     <div className="py-10 mb-10">
-      <div className="container">
-        {/* header section */}
+      <div className="container mx-auto">
+        {/* Header Section */}
         <div className="text-center mb-10 max-w-[600px] mx-auto">
           <p data-aos="fade-up" className="text-sm text-primary">
             What our customers are saying
           </p>
           <h1 data-aos="fade-up" className="text-3xl font-bold">
-          Senior Consultants
+            Senior Consultants
           </h1>
           <p data-aos="fade-up" className="text-xs text-gray-400">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
-            asperiores modi Sit asperiores modi
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit asperiores modi.
           </p>
         </div>
 
-        {/* Testimonial cards */}
+        {/* Testimonial Cards */}
         <div data-aos="zoom-in">
           <Slider {...settings}>
             {TestimonialData.map((data) => (
-              <div className="my-6">
-                <div
-                  key={data.id}
-                  className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-800 bg-primary/10 relative"
-                >
-                  <div className="mb-4">
+              <div className="my-6" key={data.id}>
+                <div className="flex flex-col lg:flex-row gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-800 bg-primary/10 relative">
+                  {/* Image Section */}
+                  <div className="flex justify-center items-center">
                     <img
                       src={data.img}
-                      alt=""
-                      className="rounded-full w-20 h-20"
+                      alt={data.name}
+                      className="w-32 h-32 object-cover" // Removed rounded-full class
                     />
                   </div>
-                  <div className="flex flex-col items-center gap-4">
-                    <div className="space-y-3">
-                      <p className="text-xs text-gray-500">{data.text}</p>
-                      <h1 className="text-xl font-bold text-black/80 dark:text-light">
-                        {data.name}
-                      </h1>
-                    </div>
+
+                  {/* Text Section */}
+                  <div className="flex flex-col justify-center items-center lg:w-1/2 space-y-3 text-center lg:text-left">
+                    <h1 className="text-xl font-bold text-black/80 dark:text-light">
+                      {data.name}
+                    </h1>
+                    <p className="text-sm text-gray-500">{data.text}</p>
                   </div>
+
                   <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
                     ,,
                   </p>
